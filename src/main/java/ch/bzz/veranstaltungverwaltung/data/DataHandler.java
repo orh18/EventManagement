ffa40086630b82f47ec07c8df2251481e12a8065
rokibulo@bzz.ch <Schule100%>
2022-05-22 22:18:52 +0200

    data

--- src/main/java/ch/bzz/veranstaltungverwaltung/data/DataHandler.java
+++ src/main/java/ch/bzz/veranstaltungverwaltung/data/DataHandler.java
@@ -1,6 +1,8 @@
 package ch.bzz.veranstaltungverwaltung.data;
 
+import ch.bzz.veranstaltungverwaltung.model.Disziplin;
 import ch.bzz.veranstaltungverwaltung.model.Teilnehmer;
+import ch.bzz.veranstaltungverwaltung.model.Veranstaltung;
 import ch.bzz.veranstaltungverwaltung.service.Config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,6 +18,8 @@ import java.util.List;
 public class DataHandler {
     private static DataHandler instance = null;
     private List<Teilnehmer> teilnehmerList;
+    private List<Disziplin> disziplinList;
+    private List<Veranstaltung> veranstaltungList;
 
     /**
      * private constructor defeats instantiation
@@ -23,6 +27,10 @@ public class DataHandler {
     private DataHandler() {
         setTeilnehmerList(new ArrayList<>());
         readTeilnehmerJSON();
+        setDisziplinList(new ArrayList<>());
+        readDisziplinJSON();
+        setVeranstaltungList(new ArrayList<>());
+        readVeranstaltungJSON();
     }
 
     /**
@@ -35,19 +43,18 @@ public class DataHandler {
         return instance;
     }
 
-
     /**
-     * reads all books
-     * @return list of books
+     * liest alle Teilnehmer
+     * @return Liste von Teilnehmer
      */
     public List<Teilnehmer> readAllTeilnehmer() {
         return getTeilnehmerList();
     }
 
     /**
-     * reads a book by its uuid
+     * liest ein Buch mit seiner uuid
      * @param teilnehmerUUID
-     * @return the Book (null=not found)
+     * @return das Buch (null=not found)
      */
     public Teilnehmer readTeilnehmerByUUID(String teilnehmerUUID) {
         Teilnehmer teilnehmer = null;
@@ -59,10 +66,54 @@ public class DataHandler {
         return teilnehmer;
     }
 
-
+    /**
+     * liest alle Disziplinen
+     * @return List von Disziplinen
+     */
+    public List<Disziplin> readAllDisziplinen() {
+        return getDisziplinList();
+    }
 
     /**
-     * reads the books from the JSON-file
+     * liest eine Disziplin mit seiner Id
+     * @param disziplinUUID
+     * @return die Disziplin (null=not found)
+     */
+    public Disziplin readDisziplinByUUID(String disziplinUUID) {
+        Disziplin disziplin = null;
+        for (Disziplin entry : getDisziplinList()) {
+            if (entry.getDisziplinUUID().equals(disziplinUUID)) {
+                disziplin = entry;
+            }
+        }
+        return disziplin;
+    }
+
+    /**
+     * liest alle Veranstaltungen
+     * @return List von Veranstaltungen
+     */
+    public List<Veranstaltung> readAllVeranstaltungen() {
+        return getVeranstaltungList();
+    }
+
+    /**
+     * liest eine Disziplin mit seiner Id
+     * @param veranstaltungUUID
+     * @return die Veranstaltung (null=not found)
+     */
+    public Veranstaltung readVeranstaltungByUUID(String veranstaltungUUID) {
+        Veranstaltung veranstaltung = null;
+        for (Veranstaltung entry : getVeranstaltungList()) {
+            if (entry.getVeranstaltungUUID().equals(veranstaltungUUID)) {
+                veranstaltung = entry;
+            }
+        }
+        return veranstaltung;
+    }
+
+    /**
+     * liest die Teilnehmer vom JSON-File
      */
     private void readTeilnehmerJSON() {
         try {
@@ -81,22 +132,94 @@ public class DataHandler {
     }
 
     /**
-     * gets teilnehmerList
+     * liest die Disziplinen vom JSON-File
+     */
+    private void readDisziplinJSON() {
+        try {
+            String path = Config.getProperty("disziplinJSON");
+            byte[] jsonData = Files.readAllBytes(
+                    Paths.get(path)
+            );
+            ObjectMapper objectMapper = new ObjectMapper();
+            Disziplin[] disziplinen = objectMapper.readValue(jsonData, Disziplin[].class);
+            for (Disziplin d : disziplinen) {
+                getDisziplinList().add(d);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * liest die Veranstaltungen vom JSON-File
+     */
+    private void readVeranstaltungJSON() {
+        try {
+            String path = Config.getProperty("veranstaltungJSON");
+            byte[] jsonData = Files.readAllBytes(
+                    Paths.get(path)
+            );
+            ObjectMapper objectMapper = new ObjectMapper();
+            Veranstaltung[] veranstaltungen = objectMapper.readValue(jsonData, Veranstaltung[].class);
+            for (Veranstaltung v : veranstaltungen) {
+                getVeranstaltungList().add(v);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * zurückgibt teilnehmerList
      *
-     * @return value of teilnehmerList
+     * @return Wert von teilnehmerList
      */
     private List<Teilnehmer> getTeilnehmerList() {
         return teilnehmerList;
     }
 
     /**
-     * sets teilnehmerList
+     * setzt disziplinList
      *
-     * @param teilnehmerList the value to set
+     * @param teilnehmerList der Wert zu setzen
      */
     private void setTeilnehmerList(List<Teilnehmer> teilnehmerList) {
         this.teilnehmerList = teilnehmerList;
     }
 
+    /**
+     * zurückgibt disziplinList
+     *
+     * @return Wert von disziplinList
+     */
+    public List<Disziplin> getDisziplinList() {
+        return disziplinList;
+    }
 
+    /**
+     * setzt disziplinList
+     *
+     * @param disziplinList der Wert zu setzen
+     */
+    public void setDisziplinList(List<Disziplin> disziplinList) {
+        this.disziplinList = disziplinList;
+    }
+
+    /**
+     * zurückgibt veranstaltungList
+     *
+     * @return Wert von veranstaltungList
+     */
+    public List<Veranstaltung> getVeranstaltungList() {
+        return veranstaltungList;
+    }
+
+    /**
+     * setzt veranstaltungList
+     *
+     * @param veranstaltungList der Wert zu setzen
+     */
+    public void setVeranstaltungList(List<Veranstaltung> veranstaltungList) {
+        this.veranstaltungList = veranstaltungList;
+    }
 }
